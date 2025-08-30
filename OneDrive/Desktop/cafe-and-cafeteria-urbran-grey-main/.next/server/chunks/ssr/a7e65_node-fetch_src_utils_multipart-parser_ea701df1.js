7e31eed194af66f7a230d9fc9e21bc1af12e27c1
charu <charup640@gmail.com>
2025-08-30 13:35:17 +0530

    Describe your changes

---
+++ OneDrive/Desktop/cafe-and-cafeteria-urbran-grey-main/.next/server/chunks/ssr/a7e65_node-fetch_src_utils_multipart-parser_ea701df1.js
@@ -0,0 +1,12 @@
+module.exports = [
+"[project]/OneDrive/Desktop/cafe-and-cafeteria-urbran-grey-main/node_modules/node-fetch/src/utils/multipart-parser.js [app-rsc] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "server/chunks/ssr/a7e65_node-fetch_src_utils_multipart-parser_8563f156.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/OneDrive/Desktop/cafe-and-cafeteria-urbran-grey-main/node_modules/node-fetch/src/utils/multipart-parser.js [app-rsc] (ecmascript)");
+    });
+});
+}),
+];
---
+++ OneDrive/Desktop/cafe-and-cafeteria-urbran-grey-main/.next/static/chunks/OneDrive_Desktop_cafe-and-cafeteria-urbran-grey-main_src_app_page_tsx_77065fbb._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/OneDrive_Desktop_cafe-and-cafeteria-urbran-grey-main_src_9c3734a1._.js",
+  "static/chunks/a7e65_848e6292._.js"
+],
+    source: "dynamic"
+});
